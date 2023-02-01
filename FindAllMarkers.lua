9084da907d38751c2e749e56597d6e1391437260
lars24515 <105982977+lars24515@users.noreply.github.com>
2023-02-01 22:08:35 +0100

    fixed it

--- FindAllMarkers.lua
+++ FindAllMarkers.lua
@@ -1,10 +1,19 @@
 local workspace = game:GetService("Workspace")
 local lp = game.Players.LocalPlayer
+local originalPos = lp.Character.HumanoidRootPart.CFrame.Position
+local originalGravity = workspace.Gravity
 
 for i,v in pairs(workspace:GetChildren()) do
+	workspace.Gravity = 0
 	if string.find(v.Name, "Marker") then
-		lp.Character.HumanoidRootPart.CFrame = CFrame.new(v:FindFirstChild("Part").Position)
-		wait(2)
-		continue
+		local part = v:FindFirstChild("Part")
+		if part then
+			lp.Character.HumanoidRootPart.CFrame = CFrame.new(part.Position)
+			wait(4)
+		end
 	end
 end
+
+print("Found all markers")
+lp.Character.HumanoidRootPart.CFrame = CFrame.new(originalPos)
+workspace.Gravity = originalGravity
